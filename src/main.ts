2cccd2dfb9126ae442f4f587c57da73d74f6c84d
MatthewKin <166259185+MatthewKin@users.noreply.github.com>
2025-10-16 16:35:10 -0700

    Added Canvas + Added Black Border (step 1 complete)

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,16 @@
-import exampleIconUrl from "./noun-paperclip-7598668-00449F.png";
+// import exampleIconUrl from "./noun-paperclip-7598668-00449F.png";
 import "./style.css";
 
-document.body.innerHTML = `
-  <p>Example image asset: <img src="${exampleIconUrl}" class="icon" /></p>
-`;
-document.body.innerHTML = `
-  <h1>hello me</h1>
-`;
+// document.body.innerHTML = `
+//   <p>Example image asset: <img src="${exampleIconUrl}" class="icon" /></p>
+// `;
+
+const appTitle = document.createElement("h1");
+appTitle.textContent = "The Really Cool And Epic Canvas";
+document.body.prepend(appTitle);
+
+const canvas = document.createElement("canvas");
+canvas.width = 256;
+canvas.height = 256;
+canvas.id = "game-canvas"; // helpful for styling or debugging
+document.body.appendChild(canvas);
